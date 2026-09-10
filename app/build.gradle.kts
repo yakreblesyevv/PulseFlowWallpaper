beb7f1325e1716d0be160f051d44f9edc5a22b40
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 21:50:51 +0300

    v0.14: bump app version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pulseflow.wallpaper.dev"
         minSdk = 26
         targetSdk = 35
-        versionCode = 13
-        versionName = "0.13"
+        versionCode = 14
+        versionName = "0.14"
     }
     buildTypes {
         release {
