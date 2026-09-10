709c9f6aebc3b8f9fa37b386db099b9f851e49fa
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 16:55:16 +0300

    Bump PulseFlow to v0.8

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pulseflow.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 7
-        versionName = "0.7"
+        versionCode = 8
+        versionName = "0.8"
     }
     buildTypes {
         release {
