59078f375a64d216832923929e6e05a02721bb27
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 16:29:21 +0300

    v0.7: bump viscous flow build version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.pulseflow.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 6
-        versionName = "0.6"
+        versionCode = 7
+        versionName = "0.7"
     }
     buildTypes {
         release {
