169dc5e5ec13970a75c96932c6761b9054d560f1
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 14:39:26 +0300

    Bump PulseFlow installable build to v0.3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.pulseflow.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 2
-        versionName = "0.2"
+        versionCode = 3
+        versionName = "0.3"
     }
 
     buildTypes {
