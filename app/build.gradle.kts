bd74fb6c6996ee75ae13bb8b41040d149ac6d795
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 17:15:45 +0300

    v0.9: create updateable dev build channel

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -7,16 +7,15 @@ android {
     namespace = "com.pulseflow.wallpaper"
     compileSdk = 35
     defaultConfig {
-        applicationId = "com.pulseflow.wallpaper"
+        applicationId = "com.pulseflow.wallpaper.dev"
         minSdk = 26
         targetSdk = 35
-        versionCode = 8
-        versionName = "0.8"
+        versionCode = 9
+        versionName = "0.9"
     }
     buildTypes {
         release {
             isMinifyEnabled = false
-            signingConfig = signingConfigs.getByName("debug")
         }
     }
     compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
