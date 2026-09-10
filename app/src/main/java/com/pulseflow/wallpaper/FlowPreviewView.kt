09b9d52f134edf64c5fe0d1471b7005839a37663
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 14:48:38 +0300

    Connect preview to live speed control

--- app/src/main/java/com/pulseflow/wallpaper/FlowPreviewView.kt
+++ app/src/main/java/com/pulseflow/wallpaper/FlowPreviewView.kt
@@ -7,6 +7,20 @@ import android.view.View
 
 class FlowPreviewView @JvmOverloads constructor(c: Context, a: AttributeSet? = null) : View(c, a) {
     private val renderer = FlowRenderer()
-    init { renderer.colors = PaletteStore.load(c) }
-    override fun onDraw(canvas: Canvas) { super.onDraw(canvas); renderer.draw(canvas); postInvalidateDelayed(33) }
+
+    init {
+        renderer.colors = PaletteStore.load(c)
+        renderer.speed = FlowSettings.loadSpeed(c)
+    }
+
+    fun setFlowSpeed(speed: Float) {
+        renderer.speed = speed
+        invalidate()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        renderer.draw(canvas)
+        postInvalidateDelayed(16)
+    }
 }
