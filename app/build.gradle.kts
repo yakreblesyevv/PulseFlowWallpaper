0fa589c0d41835f733a3c652ecfbb29472d0c031
yakreblesyevv <ylesyev@gmail.com>
2026-09-10 15:30:54 +0300

    Bump PulseFlow to v0.6

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,25 +6,19 @@ plugins {
 android {
     namespace = "com.pulseflow.wallpaper"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "com.pulseflow.wallpaper"
         minSdk = 26
         targetSdk = 35
-        versionCode = 4
-        versionName = "0.4"
+        versionCode = 6
+        versionName = "0.6"
     }
-
     buildTypes {
         release {
             isMinifyEnabled = false
             signingConfig = signingConfigs.getByName("debug")
         }
     }
-
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
+    compileOptions { sourceCompatibility = JavaVersion.VERSION_17; targetCompatibility = JavaVersion.VERSION_17 }
     kotlinOptions { jvmTarget = "17" }
 }
